9d9be8df10a101de9fe7b475a4cb940579900b3f
Gábor, Koleszár <gabor.koleszar@wup.hu>
2023-12-01 09:40:28 +0100

    Upgrade dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -55,7 +55,7 @@ dependencies {
 
 	implementation("androidx.core:core-ktx:1.12.0")
 	implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
-	implementation("androidx.activity:activity-compose:1.8.0")
+	implementation("androidx.activity:activity-compose:1.8.1")
 	implementation(platform("androidx.compose:compose-bom:2023.10.01"))
 	implementation("androidx.core:core-splashscreen:1.0.1")
 	implementation("androidx.compose.ui:ui")
@@ -81,7 +81,7 @@ dependencies {
 	implementation("com.squareup.okhttp3:logging-interceptor:4.12.0")
 
 	// Room
-	val roomVersion = "2.6.0"
+	val roomVersion = "2.6.1"
 	implementation("androidx.room:room-ktx:$roomVersion")
 	implementation("androidx.room:room-runtime:$roomVersion")
 	ksp("androidx.room:room-compiler:$roomVersion")
