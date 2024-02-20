0f70675212b409c48471b9055a654707e484cc07
Gabor, Koleszar <kgabi92@protonail.ch>
2024-02-20 19:13:35 +0100

    Update dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -54,15 +54,15 @@ android {
 dependencies {
 
 	implementation("androidx.core:core-ktx:1.12.0")
-	implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
+	implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
 	implementation("androidx.activity:activity-compose:1.8.2")
-	implementation(platform("androidx.compose:compose-bom:2023.10.01"))
+	implementation(platform("androidx.compose:compose-bom:2024.02.00"))
 	implementation("androidx.core:core-splashscreen:1.0.1")
 	implementation("androidx.compose.ui:ui")
 	implementation("androidx.compose.ui:ui-graphics")
 	implementation("androidx.compose.ui:ui-tooling-preview")
 	implementation("androidx.compose.material3:material3")
-	implementation("androidx.compose.material:material:1.5.4")
+	implementation("androidx.compose.material:material:1.6.1")
 	debugImplementation("androidx.compose.ui:ui-tooling")
 	debugImplementation("androidx.compose.ui:ui-test-manifest")
 
@@ -88,7 +88,7 @@ dependencies {
 	ksp("androidx.room:room-compiler:$roomVersion")
 
 	// Navigation
-	implementation("androidx.navigation:navigation-compose:2.7.6")
+	implementation("androidx.navigation:navigation-compose:2.7.7")
 
 	//Apache Poi
 	implementation("org.apache.poi:poi-ooxml:5.2.5")
