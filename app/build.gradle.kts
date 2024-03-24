e2ec969ea70927bf35d5796ae9f44a19deb9bf06
Gabor, Koleszar <kgabi92@protonail.ch>
2024-03-24 20:57:38 +0100

    Update dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -45,7 +45,7 @@ android {
 		compose = true
 	}
 	composeOptions {
-		kotlinCompilerExtensionVersion = "1.5.10"
+		kotlinCompilerExtensionVersion = "1.5.11"
 	}
 	packaging {
 		resources {
@@ -59,7 +59,7 @@ dependencies {
 	implementation("androidx.core:core-ktx:1.12.0")
 	implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
 	implementation("androidx.activity:activity-compose:1.8.2")
-	implementation(platform("androidx.compose:compose-bom:2024.02.01"))
+	implementation(platform("androidx.compose:compose-bom:2024.02.02"))
 	implementation("androidx.core:core-splashscreen:1.0.1")
 	implementation("androidx.compose.ui:ui")
 	implementation("androidx.compose.ui:ui-graphics")
@@ -72,7 +72,7 @@ dependencies {
 	debugImplementation("androidx.compose.ui:ui-test-manifest")
 
 	//Firebase
-	implementation(platform("com.google.firebase:firebase-bom:32.7.3"))
+	implementation(platform("com.google.firebase:firebase-bom:32.7.4"))
 	implementation("com.google.firebase:firebase-crashlytics")
 	implementation("com.google.firebase:firebase-analytics")
 
