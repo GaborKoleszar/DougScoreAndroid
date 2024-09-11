ddc249f39a85195b06707319ac2f488b609f3303
Gabor, Koleszar <gabor.koleszar@protonmail.ch>
2024-09-11 21:43:07 +0200

    Release 0.6

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
 		applicationId = "gabor.koleszar.dougscore"
 		minSdk = 26
 		targetSdk = 34
-		versionCode = 5
-		versionName = "0.3"
+		versionCode = 6
+		versionName = "0.6"
 
 		testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 		vectorDrawables {
