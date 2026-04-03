7db9ee3beaf020b09abb86698c3143d19a404df7
Gábor, Koleszár <gabor.koleszar@finshape.com>
2026-04-03 14:14:26 +0200

    feat: add ToggleManufacturerFilter, ToggleCountryFilter, and clear actions

--- app/src/main/java/gabor/koleszar/dougscore/presentation/overview/OverviewAction.kt
+++ app/src/main/java/gabor/koleszar/dougscore/presentation/overview/OverviewAction.kt
@@ -1,10 +1,14 @@
 package gabor.koleszar.dougscore.presentation.overview
 
 sealed interface OverviewAction {
-	data object ToggleIsDescending : OverviewAction
-	data object ClearSearchField : OverviewAction
-	data object RefreshList : OverviewAction
-	data object DescriptionClick : OverviewAction
-	data class CarClick(val carId: Int) : OverviewAction
-	data class SearchTextChange(val searchQuery: String) : OverviewAction
+    data object ToggleIsDescending : OverviewAction
+    data object ClearSearchField : OverviewAction
+    data object RefreshList : OverviewAction
+    data object DescriptionClick : OverviewAction
+    data object ClearManufacturerFilters : OverviewAction
+    data object ClearCountryFilters : OverviewAction
+    data class CarClick(val carId: Int) : OverviewAction
+    data class SearchTextChange(val searchQuery: String) : OverviewAction
+    data class ToggleManufacturerFilter(val manufacturer: String) : OverviewAction
+    data class ToggleCountryFilter(val country: String) : OverviewAction
 }
